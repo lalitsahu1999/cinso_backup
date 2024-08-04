b8d3a1ac05d7881925ca93a063def2f10f43402c
lalit <lalitprasad9031@gmail.com>
2024-08-04 15:50:47 +0530

    initial

---
+++ lib/ui/testOpen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Testopen extends StatefulWidget {
+  const Testopen({super.key});
+
+  @override
+  State<Testopen> createState() => _TestopenState();
+}
+
+class _TestopenState extends State<Testopen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Welcome'),
+    );
+  }
+}
